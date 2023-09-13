c146b5f1bbad9461edcf44feadfbc849767a8f74
k1nho <kinhong99@gmail.com>
2023-09-13 17:12:13 -0400

    sandboxer

---
+++ welcome.js
@@ -0,0 +1,5 @@
+const genericMsg = `Hi! welcome to sandboxer, home of the boxers`;
+const questionMsg = `Are you looking for some boxer?`;
+
+exports.genericMsg = genericMsg;
+exports.questionMsg = questionMsg;
